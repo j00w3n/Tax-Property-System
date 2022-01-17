41b052f75132aebcb882e0be8509b85c1c83aaf0
Ilyas <92800413+ilyasnawawie@users.noreply.github.com>
2022-01-17 13:34:31 +0800

    upload balik sebab juan terdelete tapi ftp dah siap

--- client.py
+++ client.py
@@ -4,10 +4,10 @@ import os
 import time
 import json
 
-
-
-
+FORMAT = "utf-8"
+SIZE = 1024
 ClientSocket = socket.socket()
+
 host = '192.168.253.12'
 port = 8888
 
@@ -19,36 +19,46 @@ try:
 except socket.error as e:
     print(str(e))
 
+welcome = ClientSocket.recv(1024) #recv 1
+print(welcome.decode(FORMAT))
 
-welcome = ClientSocket.recv(2048)
-print(welcome.decode("utf-8"))
+#Client enter Ic Number
+input_ic = input('\nPlease enter your Identity Card: ')
+ClientSocket.send(str.encode(input_ic)) #send 2 
+ResponseIC=ClientSocket.recv(1024) #recv 3
+print(ResponseIC.decode(FORMAT))
+
+#Client prompts input data
+purchase = input('Enter your Property Purchase Price: RM ')
+sale = input('Enter your Property Sale Price: RM ')
+owneryear = input ('Enter period of ownership:  ')
+#send list of data JSON to server 
+mydata = {"p": purchase,"s": sale,"o": owneryear}
+sendData = json.dumps(mydata)
+ClientSocket.send(str.encode(sendData)) #send 4
+
 while True:
-    #Client enter Ic Number
-    input_ic = input('\nPlease enter your Identity Card: ')
-    ClientSocket.send(str.encode(input_ic))
-    ResponseIC=ClientSocket.recv(2048)
-    print(ResponseIC.decode("utf-8"))
-
-    #Client prompts input data
-    purchase = input('Enter your Property Purchase Price: RM ')
-    sale = input('Enter your Property Sale Price: RM ')
-    owneryear = input ('Enter period of ownership:  ')
-    #send list of data JSON to server
-    mydata = {"p": purchase,"s": sale,"o": owneryear}
-    sendData = json.dumps(mydata)
-    ClientSocket.send(str.encode(sendData))
-
-    #ResponseCharge = ClientSocket.recv(2048)
-    #print(ResponseCharge.decode("utf-8"))
-
-    optionMenu=ClientSocket.recv(2048)
-    print(optionMenu.decode("utf-8"))
-
-    option = input('Please enter your options: ')
-    ClientSocket.send(str.encode(option))
-
-    msg = ClientSocket.recv(2048)
-    print(msg.decode("utf-8"))
-    
-ClientSocket.close()
- 
+    print("""
+                 MENU
+                 [1] Yearly property taxes
+                 [2] Print out statement
+                 [3] Exit
+                                         """)
+    option = input('Please enter your options: ') 
+    if option == '3':
+        break
+    else:
+        ClientSocket.send(str.encode(option)) #send 6
+        if option == '1':
+            msg = ClientSocket.recv(1024)
+            print(msg.decode(FORMAT))
+        if option == '2':
+            fn = ClientSocket.recv(1024).decode(FORMAT)
+            print(f"Receiving the filename.")
+            file = open(fn, "w+")
+            fd = ClientSocket.recv(1024).decode(FORMAT)
+            print(f"Receiving the file data.")
+            file.write(fd)
+            file.close()
+            
+ClientSocket.close(
--- server.py
+++ server.py
@@ -4,16 +4,19 @@ import time
 import errno
 from multiprocessing import Process
 import json
-import math
+import os
+
+
 
 ok_message = 'HTTP/1.0 200 OK\n\n'
 nok_message = 'HTTP/1.0 404 NotFound\n\n'
 
 def process_start(s_sock):
-    s_sock.send(str.encode("\n\nWELCOME TO THE TAX PROPERTY ONLINE SYSTEM"))
+    s_sock.send(str.encode("\n\nWELCOME TO THE TAX PROPERTY ONLINE SYSTEM"))#send 1 
     readIC()
     calc()
-    option() 
+    while True:
+        option() 
     s_sock.close()
 
 def animated_loading():
@@ -29,7 +32,7 @@ def animated_loading():
        done = True
 
 def readIC():
-    ic = s_sock.recv(2048)
+    ic = s_sock.recv(2048) #recv 2
     readIC.ic = ic.decode("utf-8")
     user_ic = readIC.ic[6]+readIC.ic[7]
     con_ic = int(user_ic)
@@ -44,12 +47,11 @@ def readIC():
                 answer = ("IC:" + str(readIC.ic) + "\nStatus : Foreigner \nYou are required to pay 30% of your value rate")
                 print(f"[+] {s_addr} Recognised as Foreigner")
                 
-    s_sock.send(str.encode(answer))
+    s_sock.send(str.encode(answer)) #send 3
 
 def calc():
-    data = s_sock.recv(2048)
+    data = s_sock.recv(2048) #recv 4
     data = json.loads(data.decode())
-    
     purchase = data.get("p")
     calc.buy = int(purchase) #cast value of purchase to int type
     sale = data.get("s")
@@ -58,13 +60,9 @@ def calc():
     calc.year = int(owneryear) #cast value of owner calc.year to int type
     netCharge = calc.sell - calc.buy 
     animated_loading()
-    print(f"[+] {s_addr} Data Receive")
+    print(f"[+] {s_addr} Data Receive") #ni shuk contoh
     print(f"[+] {s_addr} Making Calculation")
 
-    #if netCharge < 0:
-        #s_sock.send(str.encode(replyCharge))
-        
-    
     if readIC.nation == "citizen":
         if calc.year < 4:
             calc.taxpay = netCharge * .3
@@ -84,13 +82,7 @@ def calc():
 
 
 def option():
-    s_sock.send(str.encode("""
-                 MENU
-                 [1] Yearly property taxes
-                 [2] Print out statement
-                 [3] Exit
-                                         """))
-    op = s_sock.recv(2048)
+    op = s_sock.recv(2048) #recv 6
     op = op.decode("utf-8")
     opt = str(op)
     if opt == '1':
@@ -100,35 +92,31 @@ def option():
                 "\nYour Period of Ownership :" + str(calc.sell) + 
                 "\nYour Yearly Property Taxes Amount :" + str(calc.taxpay) + "\n")
             s_sock.send(str.encode(msg))
-        
-
-    elif opt == '3':
-            print("Closing connection")
-            sys.exit()
-            ClientSocket.close()
+    
+    elif opt == '2':
+            f = open("statement.txt","x")
+            f.write("User :" + str(readIC.ic) + "\nYour property purchase are :" + str(calc.buy) + "\nYour Property Sale are :" + str(calc.sell) + "\nYour yearly property taxes amount :" + str(calc.taxpay) +"\nPeriod of ownership : " + str(calc.year))
             f.close()
-    else:
-            print("Input not recognised")
+            f = open("statement.txt","r")
+            data = f.read()
+            s_sock.send("statement.txt".encode("utf-8"))
+            s_sock.send(data.encode("utf-8"))
+            f.close()
+            os.remove("statement.txt")
 
-#buat macam mana nak create file dekat server , and send ke client
-
-if __name__ == '__main__':
+    
+if _name_ == '_main_':
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("",8888))
     print("listening...")
     s.listen(3)
-    
     try:
         while True:
             try:
                 s_sock, s_addr = s.accept()
-                nation =""
                 print(f"[+] {s_addr} connected")
                 p1 = Process(target=process_start, args=(s_sock,))
                 p1.start()
-                p1.join()
-                
-
             except socket.error:
                 print('got a socket error')
 
